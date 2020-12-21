f092d4c26c6634295994d12d51386ccbbcd48c25
Boris Nikolov <nikolov77777@gmail.com>
2020-12-21 13:58:22 +0200

    Add RequestManager class responsible for network communication routing

---
+++ app/src/main/java/com/bnikolov/java2daysdemo/network/manager/RequestManager.kt
@@ -0,0 +1,11 @@
+package com.bnikolov.java2daysdemo.network.manager
+
+import com.bnikolov.java2daysdemo.network.service.RepositoryService
+import javax.inject.Inject
+
+class RequestManager @Inject constructor(
+    private val repositoryService: RepositoryService
+) {
+
+    fun getRepositories() = repositoryService.getRepositories()
+}
