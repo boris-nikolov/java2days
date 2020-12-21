36109aa163b3749f83875b3212ea18af786c12ef
Boris Nikolov <nikolov77777@gmail.com>
2020-12-21 02:05:41 +0200

    Create activity package

--- app/src/main/java/com/bnikolov/java2daysdemo/activity/MainActivity.kt
+++ app/src/main/java/com/bnikolov/java2daysdemo/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.bnikolov.java2daysdemo
+package com.bnikolov.java2daysdemo.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bnikolov.java2daysdemo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
