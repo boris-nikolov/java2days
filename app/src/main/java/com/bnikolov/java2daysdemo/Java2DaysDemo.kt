0755fc31211748ffc9a8c3263f7300039b58722a
Boris Nikolov <nikolov77777@gmail.com>
2020-12-22 10:27:00 +0200

    Add Realm dependency, plugin and initialisation

--- app/src/main/java/com/bnikolov/java2daysdemo/Java2DaysDemo.kt
+++ app/src/main/java/com/bnikolov/java2daysdemo/Java2DaysDemo.kt
@@ -3,6 +3,8 @@ package com.bnikolov.java2daysdemo
 import android.app.Application
 import com.bnikolov.java2daysdemo.network.manager.ConnectivityManager
 import dagger.hilt.android.HiltAndroidApp
+import io.realm.Realm
+import io.realm.RealmConfiguration
 
 @HiltAndroidApp
 class Java2DaysDemo : Application() {
@@ -11,5 +13,9 @@ class Java2DaysDemo : Application() {
         super.onCreate()
 
         ConnectivityManager.init(this)
+
+        Realm.init(this)
+        val config = RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().build()
+        Realm.setDefaultConfiguration(config)
     }
 }
